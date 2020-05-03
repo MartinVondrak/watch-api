8ee70a965837dc91b70b24d837dd2fb8abb71137
Martin Vondrák <martinvondrak@icloud.com>
2020-05-03 12:28:38 +0200

    Fix CS

--- src/Repository/MySqlWatchRepository.php
+++ src/Repository/MySqlWatchRepository.php
@@ -10,11 +10,11 @@ interface MySqlWatchRepository
 {
     /**
      * @param int $id
-     *
      * @return MySqlWatchDTO
      *
-     * @throws MySqlWatchNotFoundException
-     * @throws MySqlRepositoryException
+     * @throws MySqlWatchNotFoundException Is thrown when the watch could not be found in a mysql database, eg. watch
+     *                                     with the associated id does not exist.
+     * @throws MySqlRepositoryException May be thrown on a fatal error, such as connection to a database failed.
      */
     public function getWatchById(int $id): MySqlWatchDTO;
 }
